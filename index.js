baba40332080b38b33840d2614df6d4142dedaf6
GURUPRASAD Raghavendran <vigchi2@gmail.com>
2021-09-08 19:02:30 +0530

    Fix for Prototype Pollution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function merge(dst, ...sources) {
     for (src of sources) {
       for (let key in src) {
+        //fix for prototype pollution
+        if (key === "__proto__" || key === "constructor") continue;
         let s = src[key], d = dst[key]
         if (Object(s) == s && Object(d) === d) {
           dst[key] = merge(d, s)
--- test/test.js
+++ test/test.js
@@ -4,3 +4,8 @@ var b = {"b":{"yellow":"mango"}}
 var c = {"a":{"orange":"orange"}}
 merge(a,b,c)
 console.log(a)
+
+//Test case for prototype pollution fix
+var prototype_pollution_test = JSON.parse('{"__proto__":{"polluted":true}}')
+merge(a,prototype_pollution_test)
+console.log({}.polluted)
